5487aef4788c31e2d2a1fedaed4a874f1ee678ab
Jpwrbd <jpwrbd456@gmail.com>
2023-08-03 17:34:25 +0700

    test

--- Homework/HW06/hw06-1-basic-for-loop.cpp
+++ Homework/HW06/hw06-1-basic-for-loop.cpp
@@ -30,3 +30,21 @@
         [7] Hello World
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            printf("(%d) Hello World\n", i);
+        } else {
+            printf("[%d] Hello World\n", i);
+        }
+    }
+
+    return 0;
+}
